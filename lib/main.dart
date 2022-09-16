3779af68dba270abb0210bae1e6cd11d8b8e1780
Retroviridae <98359871+Retroviridae@users.noreply.github.com>
2022-09-15 22:38:39 -0500

    Finished part 12: Settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wurtle/pages/settings.dart';
 import 'package:wurtle/providers/controller.dart';
 import 'package:wurtle/providers/theme_provider.dart';
 import 'package:wurtle/themes/themes.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Wurtle',
         theme: notifier.isDark ? darkTheme : lightTheme,
-        home: const HomePage(),
+        home: const Settings(),
       ),
     );
   }
---
+++ lib/pages/settings.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool _isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.maybePop(context);
+              },
+              icon: const Icon(Icons.clear))
+        ],
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+              value: _isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  _isSwitched = value;
+                });
+              })
+        ],
+      ),
+    );
+  }
+}
